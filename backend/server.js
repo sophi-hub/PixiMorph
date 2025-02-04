dc3b8fc3c078488b8806ca1c551d0e3b35252a93
sophi-hub <alexconner470@gmail.com>
2025-02-03 23:00:30 -0300

    Modifying port and testing port

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,18 @@ const imageRoutes = require("./routes/imageRoutes");
 
 dotenv.config();
 const app = express();
-const PORT = process.env.DB_PORT || 3306;
+const PORT = process.env.DB_PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/images", imageRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Bem-vindo à API do PixiMorph!");
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
